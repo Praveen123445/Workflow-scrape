c9d5ad6f3380ef765925d62645a7d9a4981ddbac
Praveen Verma <Pravinvarma324@gmail.com>
2025-01-24 07:54:51 +0530

    workflow in process

---
+++ actions/workflows/createWorkflow.ts
@@ -0,0 +1,25 @@
+"use server"
+
+import prisma from "@/lib/prisma";
+import { createWorkflowSchema, createWorkflowSchemaType } from "@/schema/workflow";
+import { auth } from "@clerk/nextjs/server";
+
+export async function CreateWorkflow(form: createWorkflowSchemaType){
+    const { success, data } = createWorkflowSchema.safeParse(form);
+    if (!success) {
+        throw new Error("invalid form data")
+    }
+    const { userId } = auth();
+
+    if (!userId) {
+        throw new Error("unauthenticated");
+    }
+
+    const result = await prisma.workflow.create({
+        data: {
+            userId,
+            status:
+            definition: "TODO",
+        },
+    })
+}
