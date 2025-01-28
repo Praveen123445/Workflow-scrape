80d02f7b749ca902fdfd22a1367967f4494c510f
Praveen Verma <Pravinvarma324@gmail.com>
2025-01-28 10:34:26 +0530

    finished connection function

---
+++ lib/workflow/task/PageToHtml.tsx
@@ -0,0 +1,29 @@
+import { TaskParamType, TaskType } from "@/types/task";
+import { GlobeIcon, LucideProps } from "lucide-react";
+
+export const PageToHtmlTask = {
+  type: TaskType.PAGE_TO_HTML,
+  label: "Get html from page",
+  icon: (props: LucideProps) => (
+    <GlobeIcon className="stroke-pink-400" {...props} />
+  ),
+  isEntryPoint: false,
+  inputs: [
+    {
+      name: "Web page",
+      type: TaskParamType.BROWSER_INSTANCE,
+      helperText: "eg: https://www.google.com",
+      required: true,
+    },
+  ],
+  outputs: [
+    {
+      name: "Html",
+      type: TaskParamType.STRING,
+    },
+    {
+      name: "Web page",
+      type: TaskParamType.BROWSER_INSTANCE,
+    },
+  ],
+};
